32c5a19625aeb03d616d9af9c39480b51ad3a6b8
NavinduAmerasinghe <navinduamerasinghe@gmail.com>
2023-09-01 16:55:15 +0530

    changes done

--- safe_pass_frontend/src/api/api.js
+++ safe_pass_frontend/src/api/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   //baseURL: process.env.REACT_APP_BASE_URL,
-  baseURL: "http://localhost:8000",
+  // baseURL: "http://localhost:8000",
+  baseURL: "https://safe-pass.onrender.com",
 });
 
 export default api;
